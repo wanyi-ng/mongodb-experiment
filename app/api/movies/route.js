97ed28670c759951fea0ee2861df5a4b87543423
Wanyi Ng <wanyi.ng1975@gmail.com>
2024-04-07 15:03:49 -0400

    wip data fetch and render w/ pagination

--- app/api/movies/route.js
+++ app/api/movies/route.js
@@ -1,12 +1,12 @@
-import clientPromise from "@/db/mongodb";
+// import clientPromise from "@/db/mongodb";
 
-export async function GET(req, res) {
-  try {
-    const client = await clientPromise
-    const db = client.db("sample_mflix")
-    const movies = await db.collection("movies").find({}).limit(100).toArray()
-    return new Response(JSON.stringify(movies))
-  } catch (e) {
-    console.error(e.message)
-  }
-}
+// export async function GET(req, res) {
+//   try {
+//     const client = await clientPromise
+//     const db = client.db("sample_mflix")
+//     const movies = await db.collection("movies").find({}).limit(100).toArray()
+//     return new Response(JSON.stringify(movies))
+//   } catch (e) {
+//     console.error(e.message)
+//   }
+// }
