23eee41b5d29f2ee814749a13cc9b98dc395f5cd
Wanyi Ng <wanyi.ng1975@gmail.com>
2024-04-13 14:11:14 -0400

    remove header tag

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -5,67 +5,65 @@ import { Bars3Icon, HomeIcon, XMarkIcon } from '@heroicons/react/24/outline'
 
 export default function Navbar() {
   return (
-    <header>
-      <Disclosure as="nav" className="z-50 shadow">
-        {({ open }) => (
-          <>
-            <div className="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
-              <div className="flex justify-between h-16">
-                <Link href="/" className="flex items-center flex-shrink-0 gap-2">
-                  <HomeIcon className="w-5 h-5" /> Home
-                </Link>
-                <div className="hidden sm:ml-6 sm:flex sm:space-x-8">
-                  {/* Current: "border-indigo-500 text-gray-900", Default: "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700" */}
-                  <a
-                    href="/movies?page=1"
-                    className="inline-flex items-center px-1 pt-1 text-sm font-medium text-gray-900 border-b-2 border-indigo-500"
-                  >
-                    Movies
-                  </a>
-                  <a
-                    href="/test"
-                    className="inline-flex items-center px-1 pt-1 text-sm font-medium text-gray-500 border-b-2 border-transparent hover:border-gray-300 hover:text-gray-700"
-                  >
-                    Test
-                  </a>
-                </div>
-                <div className="flex items-center -mr-2 sm:hidden">
-                  {/* Mobile menu button */}
-                  <Disclosure.Button className="relative inline-flex items-center justify-center p-2 text-gray-400 rounded-md hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
-                    <span className="absolute -inset-0.5" />
-                    <span className="sr-only">Open main menu</span>
-                    {open ? (
-                      <XMarkIcon className="block w-6 h-6" aria-hidden="true" />
-                    ) : (
-                      <Bars3Icon className="block w-6 h-6" aria-hidden="true" />
-                    )}
-                  </Disclosure.Button>
-                </div>
-              </div>
-            </div>
-
-            <Disclosure.Panel className="sm:hidden">
-              <div className="pt-2 pb-3 space-y-1">
-                {/* Current: "bg-indigo-50 border-indigo-500 text-indigo-700", Default: "border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700" */}
-                <Disclosure.Button
-                  as="a"
+    <Disclosure as="nav" className="z-50 shadow">
+      {({ open }) => (
+        <>
+          <div className="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
+            <div className="flex justify-between h-16">
+              <Link href="/" className="flex items-center flex-shrink-0 gap-2">
+                <HomeIcon className="w-5 h-5" /> Home
+              </Link>
+              <div className="hidden sm:ml-6 sm:flex sm:space-x-8">
+                {/* Current: "border-indigo-500 text-gray-900", Default: "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700" */}
+                <a
                   href="/movies?page=1"
-                  className="block py-2 pl-3 pr-4 text-base font-medium text-indigo-700 border-l-4 border-indigo-500 bg-indigo-50"
+                  className="inline-flex items-center px-1 pt-1 text-sm font-medium text-gray-900 border-b-2 border-indigo-500"
                 >
                   Movies
-                </Disclosure.Button>
-                <Disclosure.Button
-                  as="a"
+                </a>
+                <a
                   href="/test"
-                  className="block py-2 pl-3 pr-4 text-base font-medium text-gray-500 border-l-4 border-transparent hover:border-gray-300 hover:bg-gray-50 hover:text-gray-700"
+                  className="inline-flex items-center px-1 pt-1 text-sm font-medium text-gray-500 border-b-2 border-transparent hover:border-gray-300 hover:text-gray-700"
                 >
                   Test
+                </a>
+              </div>
+              <div className="flex items-center -mr-2 sm:hidden">
+                {/* Mobile menu button */}
+                <Disclosure.Button className="relative inline-flex items-center justify-center p-2 text-gray-400 rounded-md hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+                  <span className="absolute -inset-0.5" />
+                  <span className="sr-only">Open main menu</span>
+                  {open ? (
+                    <XMarkIcon className="block w-6 h-6" aria-hidden="true" />
+                  ) : (
+                    <Bars3Icon className="block w-6 h-6" aria-hidden="true" />
+                  )}
                 </Disclosure.Button>
               </div>
-            </Disclosure.Panel>
-          </>
-        )}
-      </Disclosure>
-    </header>
+            </div>
+          </div>
+
+          <Disclosure.Panel className="sm:hidden">
+            <div className="pt-2 pb-3 space-y-1">
+              {/* Current: "bg-indigo-50 border-indigo-500 text-indigo-700", Default: "border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700" */}
+              <Disclosure.Button
+                as="a"
+                href="/movies?page=1"
+                className="block py-2 pl-3 pr-4 text-base font-medium text-indigo-700 border-l-4 border-indigo-500 bg-indigo-50"
+              >
+                Movies
+              </Disclosure.Button>
+              <Disclosure.Button
+                as="a"
+                href="/test"
+                className="block py-2 pl-3 pr-4 text-base font-medium text-gray-500 border-l-4 border-transparent hover:border-gray-300 hover:bg-gray-50 hover:text-gray-700"
+              >
+                Test
+              </Disclosure.Button>
+            </div>
+          </Disclosure.Panel>
+        </>
+      )}
+    </Disclosure>
   )
 }
