14c595b2e9f316b26ade39892bc4e6e345434c6b
Wanyi Ng <wanyi.ng1975@gmail.com>
2024-04-15 19:07:05 -0400

    feed search term to router url and useDebounceCallback

--- components/movies/Filters.jsx
+++ components/movies/Filters.jsx
@@ -156,7 +156,7 @@ export default function Filters() {
           </h2>
 
           <div className="flex items-center justify-between gap-8">
-            <Search />
+            <Search placeholder={"Search..."} />
 
             <button
               type="button"
